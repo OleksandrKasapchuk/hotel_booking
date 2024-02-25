721983c009ce1cfb0ccbb4df8b56f9ab5785d6b3
OleksandrKasapchuk <sasha291108@gmail.com>
2024-02-25 02:34:25 +0200

    added starting page

--- booking/urls.py
+++ booking/urls.py
@@ -4,6 +4,6 @@ import booking.views as booking_views
 urlpatterns = [
     path("", booking_views.index, name="index"),
     path("rooms/", booking_views.show_rooms, name="rooms"),
-    path("rooms/<int:room_id>", booking_views.get_room, name="room_details"),
+    path("rooms/<int:room_id>", booking_views.get_room, name="room-details"),
     path("search-room/", booking_views.search_room, name="search-room")
 ]
