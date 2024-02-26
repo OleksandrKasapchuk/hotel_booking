dbf2f269331f45166669216972331a1fe5da730a
OleksandrKasapchuk <sasha291108@gmail.com>
2024-02-26 22:29:19 +0200

    added booking option

--- booking/models.py
+++ booking/models.py
@@ -5,7 +5,6 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.EmailField()
-
     def __str__(self):
         return self.name
 
