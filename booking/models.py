1b4aa4b79084c8fa921535ddd2e294bf02b2cdad
OleksandrKasapchuk <sasha291108@gmail.com>
2024-02-27 23:12:14 +0200

    added booking option

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import *
 
 class User(models.Model):
     name = models.CharField(max_length=50)
@@ -12,11 +12,12 @@ class Room(models.Model):
     number = models.IntegerField()
     capacity = models.IntegerField()
     location = models.TextField()
+    available = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = "Room"
         verbose_name_plural = "Rooms"
-        ordering = ["number", "-capacity"]
+        ordering = ["number", "available"]
 
     def __str__(self):
         return f"Room #{self.number} for {self.capacity}"
@@ -28,11 +29,11 @@ class Booking(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     creation_time = models.DateTimeField(auto_now_add=True)
-
+    
     class Meta:
         verbose_name = "Booking"
         verbose_name_plural = "Bookings"
-        ordering = ["start_time"]
+        ordering = ["start_time", "creation_time"]
     
     def __str__(self):
         return f"{self.user} - {self.room}"
