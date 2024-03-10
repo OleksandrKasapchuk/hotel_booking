cb060441770b942d44ea9c19591f2b1773a42e7a
sashapre228 <sasha291108@gmail.com>
2024-03-10 16:33:59 +0100

    created second app

--- booking/admin.py
+++ booking/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from booking.models import Booking, Room
+from booking.models import Booking, Room, Category
 
 admin.site.register(Room)
+admin.site.register(Category)
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     list_display =("user", "room", "start_time", "end_time")
